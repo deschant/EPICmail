ac61d661ec0ee06a2454431cf4c0466cdc7b7a29
Deschant <kounoudeschant@gmail.com>
2019-02-25 17:52:19 +0100

    Autofill email input field in signup form

--- js/main.js
+++ js/main.js
@@ -31,9 +31,12 @@ const toggleForms = ($formId) => {
  * Handles password and password confirmation checking
  */
 const isSignupForm = () => {
-  console.log("We got a sign up form!");
+  const $signupBtn = document.getElementById('signup-btn');
   const $password = document.getElementById('password');
   const $confirmPassword = document.getElementById('confirm-password');
+  const $firstName = document.getElementById('fN');
+  const $lastName = document.getElementById('lN');
+  const $email = document.getElementById('email-input');
 
   const validatePassword = () => {
     if ($password.value !== $confirmPassword.value) {
@@ -42,7 +45,11 @@ const isSignupForm = () => {
       $confirmPassword.setCustomValidity('');
     }
   };
+  $signupBtn.onclick = validatePassword;
 
-  $password.onchange = validatePassword;
-  $confirmPassword.onchange = validatePassword;
+  // Autofill email field with concatenated firstName and lastName
+  $email.onclick = () => {
+    // @TODO check that this combination of firstName and lastName is unique in the backend before suggesting. if it is not then append random strings to make it so.
+    $email.value = $firstName.value + $lastName.value;
+  }
 };
