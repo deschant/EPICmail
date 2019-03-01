5f9539353bdc3ff36da880ceed3d08714ad5267e
Deschant <kounoudeschant@gmail.com>
2019-03-01 19:49:43 +0100

    [Feature #164231561] Add password reset page flow

--- ui/js/password.js
+++ ui/js/password.js
@@ -7,27 +7,11 @@
 const handlePasswordForgot = () => {
   const countryCode = document.getElementById('country-code').value;
   const phoneNumber = document.getElementById('phone-input').value;
-  const $resetButton = document.getElementById('reset-password');
-  const $passwordForgot = document.getElementById('password-forgot');
-  const $newPassword = document.getElementById('new-password');
-  const $noAccount = document.getElementById('account-nomatch');
+  const resetButton = document.getElementById('reset-password');
+  const passwordForgot = document.getElementById('password-forgot');
+  const newPassword = document.getElementById('new-password');
+  const noAccount = document.getElementById('account-nomatch');
 
-  
-
-  // Add loading icon to button
-  $resetButton.innerHTML = "Reset Password&nbsp;<i class='fas fa-circle-notch fa-spin'></i>"
-  // @TODO make AJAX call to backend to check if the number is associated with an account.
-  // @TODO if it is toggle password reset form
-    // $passwordForgot.style.display = 'none';
-    // $newPassword.style.display = 'flex';
-  
-
-  // @TODO if AJAX call does not find account then display failed icon with red text
-  $resetButton.innerHTML = "Reset Password&nbsp; <i class='fas fa-ban'></i>"
-  $noAccount.style.display = 'inline';
+  passwordForgot.style.display = 'none';
+  newPassword.style.display = 'flex';
 };
-
-/**
- * Handle new password submission
- * Makes AJAX call for new password
- */
