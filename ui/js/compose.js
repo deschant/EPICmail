09cd5c7b0d35654a927d13c90cde63c931eec5d9
Deschant <kounoudeschant@gmail.com>
2019-03-11 20:46:05 +0100

    [Chore #164551231] Remove Quill.js wysiwyg editor and use simple text input

--- ui/js/compose.js
+++ ui/js/compose.js
@@ -1,11 +1,4 @@
-// Quill configuration
-const container = document.getElementById('mail-editor');
-const quillOptions = {
-  theme: 'snow',
-  placeholder: 'Start crafting your epic mail here'
-};
-const editor = new Quill(container, quillOptions);
-
+const editor = document.getElementById('mail-editor');
 const sendMail = (isDraft) => {
   // Get all input fields and prepare data for sending mail
   const to = document.getElementById('to');
@@ -20,7 +13,5 @@ const sendMail = (isDraft) => {
     draft = editor.getContents();
     alert(JSON.stringify(draft));
   }
-
-  // Make api call to save email
 };
 
